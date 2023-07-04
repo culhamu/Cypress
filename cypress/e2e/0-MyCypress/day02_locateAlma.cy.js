86003c6f1807c62dc9b19a403157c3e3acc59e50
Mustafa <merguven75@mail.com>
2023-07-04 22:39:29 +0300

    04.07 mustafa

--- cypress/e2e/0-MyCypress/day02_locateAlma.cy.js
+++ cypress/e2e/0-MyCypress/day02_locateAlma.cy.js
@@ -4,4 +4,7 @@ describe('Locate alma', () => {
         cy.get('#APjFqb').type('cypress{enter}')
         cy.get('div#hdtb-msb div:nth-child(2) > a').click
     });
+    // xpath-->  //input[@id='APjFqb']     ///   cy.get('//input[@id="APjFqb"]')
+//cy.get komutunda id alınıyorsa cy.get('id[APjFqb']) yerine cy.get('#APjFqb') yani id ise # koymak yeterli
+//class almışsak onun içinde . koymak yeterli
 });
