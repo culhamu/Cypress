1ee1afcb192bd973d39ad9cf2d7654c1ffb76b51
culhamu <mustafa.qatester@gmail.com>
2023-07-14 20:29:25 +0300

    14.07 Mustafa

--- cypress/e2e/0-MyCypress/day07/Read&Assert.cy.js
+++ cypress/e2e/0-MyCypress/day07/Read&Assert.cy.js
@@ -2,6 +2,8 @@ describe('Read And Assert form files', () => {
     it('Read and Assert', () => {
         cy.readFile('cypress/fixtures/user.json').then((userdata) => {
             expect(userdata[7].name).to.equal('Nicholas Runolfsdottir V')
+            expect(userdata[7].username).to.equal('Maxime_Nienow')
+            expect(userdata[7].email).to.equal('Sherwood@rosamond.me')
         })
     });
 });
