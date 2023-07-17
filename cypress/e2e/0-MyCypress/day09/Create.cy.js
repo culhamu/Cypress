fa1a026635235bfb9352c9cceddc1334c013012f
culhamu <mustafa.qatester@gmail.com>
2023-07-17 21:28:47 +0300

    17.07 Mustafa

--- cypress/e2e/0-MyCypress/day09/Create.cy.js
+++ cypress/e2e/0-MyCypress/day09/Create.cy.js
@@ -1,24 +1,23 @@
-
-describe('POST Create', () => {
-
-    const bodyRequest={"name":"Murat","salary":20000}
-    it('Test', () => {
-        
+escribe('Create POST', () => {
+    const bodyrequest={"name":"Aysel","salary":"3500","age":"23"}
+    it('TEST', () => {
         cy.request({
-            url:'https://dummy.restapiexample.com/api/v1/create',
-            method:'POST',
-            body:{"name":"Muhsin","salary":20000,"age":30}
-
+            url: 'https://dummy.restapiexample.com/api/v1/create',
+            method: 'POST',
+            body :bodyrequest
+        }).then(response =>{
+            expect(response.body.message).to.eq("Successfully! Record has been added.")
+            expect(response.body.data.name).to.eq("Aysel")
         })
-    });
-
-    it('Test 02', () => {
         
-        cy.request({
-            url:'https://dummy.restapiexample.com/api/v1/create',
-            method:'POST',
-            body:bodyRequest
-
-        }).then
     });
 });
+
+
+
+
+
+
+
+
+
